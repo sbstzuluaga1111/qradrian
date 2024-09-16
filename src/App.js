34919b682dc936f14fc0aca54a5dda7b3f8bc9a8
sbstzuluaga1111 <sbstzuluagajob@gmail.com>
2024-09-15 21:52:02 -0500

    v1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
+import ima1 from './1.png';
+import ima2 from './2.png';
+import ima3 from './3.png'; 
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <h1>Site Web</h1>
+
+        <div className='center'>
+        <img src={ima1} alt="club bucarabike 1" />
+        <p>@ clupbucarabike</p>
+        </div>
+
+ <div className='center'>
+        <img src={ima2} alt="club bucarabike 2" />
+        <p>@ clupbucarabike</p>  </div>
+
+<div className='center'>       
+        <img src={ima3} alt="club bucarabike 3" />
+        <p>@ clupbucarabike</p>  </div>
       </header>
     </div>
   );
